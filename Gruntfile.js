bfa2c12910fca8bf191794c1bd70abcd09d2370f
Aitor García <aitor.falc@gmail.com>
2014-11-09 13:40:18 +0100

    Add Gruntfile.js.

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('bower.json'),
+    jshint: {
+      grunt: {
+        src: ['Gruntfile.js']
+      },
+      main: {
+        src: ['tock.js']
+      },
+    },
+    uglify: {
+      options: {
+        banner: '// Tock.js (version <%= pkg.version %>) <%= pkg.homepage %>\n// License: <%= pkg.license %>\n'
+      },
+      main: {
+        src: '<%= jshint.main.src %>',
+        dest: 'tock.min.js'
+      }
+    },
+    watch: {
+      main: {
+        files: '<%= jshint.main.src %>',
+        tasks: ['jshint:main', 'uglify:main']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['jshint', 'uglify']);
+};
