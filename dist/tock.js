97826eb023b33b202a45dedc07913cdd34a819ee
Aitor García <aitor.falc@gmail.com>
2019-12-17 10:01:57 +0100

    Update dist files

--- dist/tock.js
+++ dist/tock.js
@@ -1,7 +1,7 @@
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module unless amdModuleId is set
-    define('tock-js', [], function () {
+    define('Tock.js', [], function () {
       return (root['Tock.js'] = factory());
     });
   } else if (typeof module === 'object' && module.exports) {
